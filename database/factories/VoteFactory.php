a7bb44777a4769f1627e0258e4eab75294ec0196
Elias Johansson <elias_johansson@hotmail.se>
2019-01-04 18:04:56 +0100

    Added vote factory

--- database/factories/VoteFactory.php
+++ database/factories/VoteFactory.php
@@ -1,9 +1,16 @@
 <?php
 
+use App\Post;
+use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(App\Vote::class, function (Faker $faker) {
+    $post = factory(Post::class)->create();
     return [
-        //
+        'type' => $faker->randomElement(['like', 'dislike']),
+        'user_id' => factory(User::class)->create()->id,
+        'voted_id' => $post->id,
+        'voted_type' => get_class($post),
+
     ];
 });
