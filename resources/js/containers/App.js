3de3c79c9e4911cd727e2c9af42302f29159ee3c
Elias Johansson <elias_johansson@hotmail.se>
2019-01-07 14:31:47 +0100

    Frontend setup, easy-peasy and router

--- resources/js/containers/App.js
+++ resources/js/containers/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { StoreProvider } from 'easy-peasy';
+import store from '../store';
 
-import Example from '../components/Example';
+const PageOne = () => <h1>Page One</h1>;
+const PageTwo = () => <h1>Page Two</h1>;
 
 const App = () => {
   return (
-    <div>
-      <h1>React App</h1>
-      <Example />
-    </div>
+    <StoreProvider store={store}>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/one">Page One</Link>
+            </li>
+            <li>
+              <Link to="/two">Page Two</Link>
+            </li>
+          </ul>
+          <Route path="/one" component={PageOne} />
+          <Route path="/two" component={PageTwo} />
+        </div>
+      </Router>
+    </StoreProvider>
   );
 };
 
