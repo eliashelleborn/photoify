f952f7df4c12a1e5941cfd7d6cb21690987d5f23
Elias Johansson <elias_johansson@hotmail.se>
2019-01-16 11:38:23 +0100

    Button click styling and LinkButton version

--- resources/js/components/Button.js
+++ resources/js/components/Button.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const Button = styled.button`
   display: block;
@@ -23,6 +24,37 @@ export const Button = styled.button`
     background-color: #fcfcfc;
   }
   &:focus {
+    box-shadow: 0 2px 3px 0px rgba(167, 167, 167, 0);
+    background-color: #f2f2f2;
+    outline: none;
+  }
+`;
+
+export const LinkButton = styled(Link)`
+  display: block;
+  font-size: 16px;
+  padding: 15px 20px;
+  margin: 0 auto;
+  margin-top: 20px;
+  margin-bottom: 10px;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+  font-weight: bold;
+  text-transform: uppercase;
+  background-color: #fff;
+  box-shadow: 0 3px 4px 1px rgba(167, 167, 167, 0.5);
+  color: inherit;
+  -webkit-tap-highlight-color: transparent;
+  transition: 0.2s ease-in-out;
+  text-decoration: none;
+  &:hover {
+    box-shadow: 0 2px 3px 0px rgba(167, 167, 167, 0.5);
+    background-color: #fcfcfc;
+  }
+  &:focus {
+    box-shadow: 0 2px 3px 0px rgba(167, 167, 167, 0);
+    background-color: #f2f2f2;
     outline: none;
   }
 `;
